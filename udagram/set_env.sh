4f08063e76c908ad1c652f8b7c4d1bc97987e8e0
DESKTOP-E05M1N3\PC <tuandaotien@gmail.com>
2024-05-26 22:52:14 +0700

    update config to deploy

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -4,6 +4,7 @@ export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Udacity2024
 export POSTGRES_HOST=database-1.cwbhfpb82r2t.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
+export POSTGRES_PORT=5432
 export AWS_BUCKET=arn:aws:s3:::tuandt14-udacity
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
