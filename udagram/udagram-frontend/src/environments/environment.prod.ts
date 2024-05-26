435c04d9e4400356ffc7e48d24fc8717ecd5bc0c
DESKTOP-E05M1N3\PC <tuandaotien@gmail.com>
2024-05-26 23:32:48 +0700

    update lint issues

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-7sah5ipk.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-7sah5ipk.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
